fa54b15255348a466333212d5210d8d0528bf873
vadimchaikin <chaikinvadimv@icloud.com>
2022-03-13 13:56:01 +0600

    Initial Commit

---
+++ Shared/test_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_appApp.swift
+//  Shared
+//
+//  Created by Вадим Чайкин on 13/3/22.
+//
+
+import SwiftUI
+
+@main
+struct test_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
